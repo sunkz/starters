b7e2b7b256c1f9b97238cf07bfc7a02d0655d45c
kezheng.sun <kezheng.sun@xiaoying.com>
2025-04-03 15:19:44 +0800

    2025-04-03 15:19:44

--- log/src/main/java/com/sunkz/starters/log/config/LogAutoConfiguration.java
+++ log/src/main/java/com/sunkz/starters/log/config/LogAutoConfiguration.java
@@ -1,6 +1,7 @@
 package com.sunkz.starters.log.config;
 
 import com.sunkz.starters.log.aop.LogAspect;
+import com.sunkz.starters.log.aop.LogCurlAspect;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,12 @@ public class LogAutoConfiguration {
     @ConditionalOnMissingBean
     public LogAspect logAspect() {
         return new LogAspect();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public LogCurlAspect logCurlAspect() {
+        return new LogCurlAspect();
     }
 
 }
